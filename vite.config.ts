ba216458eeed16802abfcec24f18f4dbf83756ba
Otmane33 <97040425+Otmane33@users.noreply.github.com>
2025-03-13 02:05:56 +0000

    Ajustement de la configuration pour Netlify

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/asso-autisme/",
+  // Si vous voulez déployer à la fois sur GitHub Pages et Netlify,
+  // vous pouvez utiliser une condition basée sur l'environnement
+  base: process.env.GITHUB_PAGES === "true" ? "/asso-autisme/" : "/",
 });
